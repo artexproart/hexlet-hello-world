e697b8a4e574544092e8afe7e2cfdeb636a13494
Artex <kirillovaaru@yandex.ru>
2025-07-03 12:47:48 +0300

    Исправлен стиль кода с помощью ruff, добавлен ruff.toml

--- hexlet_hello_world/example.py
+++ hexlet_hello_world/example.py
@@ -1,5 +1,6 @@
 print("Hello from python project!")
 
+
 # hexlet_hello_world/example.py
 def example_function():
     print("Пример функции из example.py")
--- hexlet_hello_world/module.py
+++ hexlet_hello_world/module.py
@@ -1,4 +1,8 @@
+# hexlet_hello_world/module.py
+
 from more_itertools import sliced, substrings
+
+from hexlet_hello_world.example import example_function
 
 subs = ["".join(s) for s in substrings("more")]
 print(subs)
@@ -6,12 +10,11 @@ print(subs)
 slices = list(sliced((1, 2, 3, 4, 5, 6, 7, 8), 3))
 print(slices)
 
-# hexlet_hello_world/module.py
-from hexlet_hello_world.example import example_function
 
 def main():
     print("Запуск приложения")
     example_function()
+
 
 if __name__ == "__main__":
     main()
