6def1504a67d2908c4f99c95dcdb81345bb88ca5
Artex <kirillovaaru@yandex.ru>
2025-07-03 12:34:52 +0300

    Добавлена точка входа, фасад библиотеки и настройка build-system

---
+++ hexlet_hello_world/__init__.py
@@ -0,0 +1,4 @@
+from .example import example_function
+
+__all__ = ("example_function",)
+
---
+++ hexlet_hello_world/module.py
@@ -0,0 +1,18 @@
+from more_itertools import sliced, substrings
+
+subs = ["".join(s) for s in substrings("more")]
+print(subs)
+
+slices = list(sliced((1, 2, 3, 4, 5, 6, 7, 8), 3))
+print(slices)
+
+# hexlet_hello_world/module.py
+from hexlet_hello_world.example import example_function
+
+def main():
+    print("Запуск приложения")
+    example_function()
+
+if __name__ == "__main__":
+    main()
+
